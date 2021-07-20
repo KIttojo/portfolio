dfa1f7f6d006ed93cea4be051d8354f264115e05
Kittojo_laptop <45813478+KIttojo@users.noreply.github.com>
2021-07-20 15:53:30 +0300

    Few text changes

--- src/Bio.js
+++ src/Bio.js
@@ -4,22 +4,22 @@ function Bio() {
       <div className="container">
         <div className="row">
           <h2 className="title"><strong>Обо мне</strong></h2>
-          <article className="lead">
+          <article className="lead p-bio">
             <p>
             Бета-тестировщик ВКонтакте а также бывший участник Экспертов Вконтакте (о последнем мало информации в интернете, так что можете ознакомиться по ссылке <a target="_blank" rel="noreferrer" href="https://kod.ru/tematicheskaya-lenta-vkontakte-2020/">клац</a>).
             </p>
             <p>
-            Имею опыт в тестировании web-приложений как на мобильных девайсах, так и десктоп. Помимо программирования люблю разрабатывать интерфейсы приложений (Figma, Photoshop). Некоторые из моих работ представлены здесь - <a target="_blank" rel="noreferrer" href="https://github.com/KIttojo">GitHub</a>, а репозиторий этого веб-приложения <a target="_bla
-            " rel="noreferrer" href="https://github.com/KIttojo/portfolio">здесь</a>.
+            Имею опыт в коммерческом тестировании web-приложений как на мобильных девайсах, так и десктоп. Помимо программирования люблю разрабатывать интерфейсы приложений (Figma, Photoshop). Некоторые из моих работ представлены здесь - <a target="_blank" rel="noreferrer" href="https://github.com/KIttojo">GitHub</a>, а репозиторий этого веб-приложения можно изучить <a target="_blank" rel="noreferrer" href="https://github.com/KIttojo/portfolio">тут</a>.
             </p>
             <p>
-            До этого года значительную часть времени уделял разработке программ на языке Python, однако в последний год перешел во front-end разработку. Принимая участие в различных проектах, приобрел обыт в испльзовании EmberJS, NodeJS, Neo4j.
+            До этого года значительную часть времени уделял разработке программ на языке Python, однако в последний год перешел во front-end разработку. Принимая участие в различных проектах, приобрел опыт в испльзовании Ember.js, Node.js, Neo4j.
             </p>
             <p>
             Итого, из списка hard-skills могу выделить владение следующими ЯП и стеком технологий:
             </p>
             <ul>
               <li>HTML, CSS, JS</li>
+              <li>Bootstrap</li>
               <li>Node.js</li>
               <li>ReactJS</li>
               <li>Python</li>
